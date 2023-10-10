ce21f73277a41749ced79deb1285838d760793f7
Awoms Kelechi <awomskelechi@gmail.com>
2023-10-10 10:33:12 +0100

    installed dependencies

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+const URL = "https://api.dictionaryapi.dev/api/v2/entries/en"
+
+function App() {
+
+  return (
+    <>
+      <p>React and typescript</p>
+    </>
+  )
+}
+
+export default App
